eef18015895f22880d79ae03249840766aa7eb37
Zhang Chengrong <zhangchengrong@yuewen.com>
2017-06-29 15:57:23 +0800

    引入vue-router，新建页面

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import Hello from './pages/hello.vue';
+// import World from './pages/world.vue';
+const World = resolve => require(['./pages/world.vue'], resolve); // 按需加载页面
+
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  router: new VueRouter({
+    mode: 'history',
+    routes: [
+      { path: '/Hello', component: Hello },
+      { path: '/world', component: World },
+    ],
+    scrollBehavior() {
+      return { x: 0, y: 0 };
+    },
+  }),
+  render: h => h(App),
+});
---
+++ src/pages/hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Hello, {{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeRouteEnter(to, from, next) {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then((result) => {
+        next((vm) => {
+          vm.name = result.name;
+        });
+      })
+  },
+  data () {
+    return {
+      name: '',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
